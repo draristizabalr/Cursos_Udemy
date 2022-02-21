d89a4f5b4f23bfa22fafb6682169c132289693a9
draristizabalr <draristizabalr@unal.edu.co>
2022-02-21 16:37:27 -0500

    Funcional con un solo componente

---
+++ Universidad_JavaScript/Angular/app-calculadora-2/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Aplicacion Calculadora 2';
+
+  firstNumber:number = 0;
+  secondNumber:number = 0;
+  resultado:number = 0;
+
+  sumar():void{
+    this.resultado = this.firstNumber + this.secondNumber;
+    console.log(this.firstNumber);
+    console.log(this.secondNumber);
+  }
+
+}
